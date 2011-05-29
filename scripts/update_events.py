9c9b767a208c6fe5e629ba6146f55eca59366219
Zach Seifts <zach.seifts@gmail.com>
2011-05-29 18:16:43 -0400

    adding in scripts

---
+++ scripts/update_events.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+class Main(object):
+    ''' Updates the events listing for the android app.
+
+    '''
+
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    main = Main()
+
